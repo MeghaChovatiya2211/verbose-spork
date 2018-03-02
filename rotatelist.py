4a814660d052a070e3b40c5b11d080ad96f68a48
MeghaChovatiya2211 <30754852+MeghaChovatiya2211@users.noreply.github.com>
2018-03-03 00:58:00 +0530

    Update rotatelist.py

--- rotatelist.py
+++ rotatelist.py
@@ -1,5 +1,5 @@
 #python program to rotate a list left based on a number entered by a user
-#write a python program to rotate list
+##write a python program to rotate list
 
 def rotatelist(l,k):
 
@@ -10,16 +10,18 @@ def rotatelist(l,k):
         l[0]=temp
         
     return(l)
-
+#main program
 k=int(input("enter a number to rotate list :"))
 size=int(input("enter size of a list :"))
 print("enter list elements : ")
 l=[]
 
+#input list elements frm user
 for i in range(0,size):
     temp1=input()
     l.append(temp1)
 
+#call function 
 print("entered list is :" +str(l))
 list1=rotatelist(l,k)
 print("list after rotation of "+str(k)+" number of times is "+str(list1))
